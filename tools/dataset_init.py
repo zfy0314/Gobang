d354f84610755fe586e2361d1a1047b961973f88
zfy0314 <zhufeiyu0314@yeah.net>
2018-11-30 22:29:36 +0800

    gobang

--- tools/dataset_init.py
+++ tools/dataset_init.py
@@ -9,7 +9,7 @@ from time import time
 
 #--global variables------------
 PATH_TO_RAW_DATA = 'data_raw/'
-DATASET_FILE = 'data/human_data.txt'
+DATASET_FILE = 'dataset/human_data.txt'
 BOARD_SIZE = 19
 
 #--helper functions------------
@@ -29,6 +29,19 @@ def zfy_rotate(matrix, num):
     return result
 
 def data_write(FILE, board):
+    mmax = max(max(board))
+    x = max(board).index(mmax)
+    y = board[x].index(mmax)
+    fout = open(FILE, 'a+')
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if 0 == board[i][j]: fout.write('0 ')
+            elif mmax == board[i][j]: fout.write('0 ')
+            else: board[i][j] = fout.write(str(2 - int((mmax % 2) == (board[i][j] % 2))) + ' ')
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if mmax == board[i][j]: fout.write('1 ')
+            else: fout.write('0 ')
     return None
 
 #--functions-------------------
@@ -36,14 +49,12 @@ def txt_read(FILE):
     with open(FILE, 'r') as fin:
         raw = fin.read()
     data = [x.replace('\r', '') for x in raw.split('\n') if not '' == x]
-    temp = data[0].split(' ')
-    mode = int(temp[0])
 
     board_u = [[int(x) for x in data[i] if not ' ' == x] 
-               for i in range(1, len(data))]
+               for i in range(len(data))]
     step_total = max(max(board_u))
     result = [board_min(board_u, 2 * i + 1) 
-              for i in range(step_total // 2 + 2 - mode)]
+              for i in range(step_total // 2 + 2 - (step_total % 2))]
     '''
     print(board_u)
     print(mode)
@@ -53,15 +64,18 @@ def txt_read(FILE):
 
 def dataset_write(FILE, BOARDS):
     size = [len(BOARDS[0]), len(BOARDS[0][0])]
-    boards = []
+    print(size)
     for brd in BOARDS:
         for i in range(1, BOARD_SIZE - size[0] - 1):
             for j in range(1, BOARD_SIZE - size[1] - 1):
                 temp = [[0] * BOARD_SIZE] * BOARD_SIZE
-                temp[i:i+size(0), j:j+size(1)] = brd
+                for l in range(size[0]):
+                    for k in range(size[1]):
+                        temp[i+l][j+k] = brd[l][k]
                 for l in [0, 1, 2, 3]:
-                    data_write(FILE, zfy_rotate(temp, l))
-                
+                    #data_write(FILE, zfy_rotate(temp, l))
+                     print(temp)
+                     print(brd)
                 
     
 #--main------------------------
