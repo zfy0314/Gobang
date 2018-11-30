31862feb64a009b259a263a39c1ea622e579c215
zfy0314 <zhufeiyu0314@yeah.net>
2018-12-01 00:18:30 +0800

    finish dataset_init

--- tools/dataset_init.py
+++ tools/dataset_init.py
@@ -9,7 +9,7 @@ from time import time
 
 #--global variables------------
 PATH_TO_RAW_DATA = 'data_raw/'
-DATASET_FILE = 'dataset/human_data.txt'
+DATASET_FILE = 'dataset/data_human.txt'
 BOARD_SIZE = 19
 
 #--helper functions------------
@@ -29,54 +29,58 @@ def zfy_rotate(matrix, num):
     return result
 
 def data_write(FILE, board):
-    mmax = max(max(board))
-    x = max(board).index(mmax)
-    y = board[x].index(mmax)
+    [x, y, mmax] = zfy_max(board)
     fout = open(FILE, 'a+')
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
             if 0 == board[i][j]: fout.write('0 ')
             elif mmax == board[i][j]: fout.write('0 ')
-            else: board[i][j] = fout.write(str(2 - int((mmax % 2) == (board[i][j] % 2))) + ' ')
+            else: fout.write(str(2 - int((mmax & 1) == (board[i][j] % 2))) + ' ')
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
             if mmax == board[i][j]: fout.write('1 ')
             else: fout.write('0 ')
+    fout.write('\n')
+    fout.close()
     return None
+
+def zfy_print(matrix):
+    for x in matrix: print(x)
+    print('\n')
+    return None
+
+def zfy_max(array):
+    x, y = -1, -1
+    mmax = 0
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            if array[i][j] > mmax: x, y, mmax = i, j, array[i][j]
+    return [x, y, mmax]
 
 #--functions-------------------
 def txt_read(FILE):
     with open(FILE, 'r') as fin:
         raw = fin.read()
     data = [x.replace('\r', '') for x in raw.split('\n') if not '' == x]
-
     board_u = [[int(x) for x in data[i] if not ' ' == x] 
                for i in range(len(data))]
-    step_total = max(max(board_u))
+    step_total = zfy_max(board_u)[2]
     result = [board_min(board_u, 2 * i + 1) 
               for i in range(step_total // 2 + 2 - (step_total % 2))]
-    '''
-    print(board_u)
-    print(mode)
-    print(result)
-    '''
     return result
 
 def dataset_write(FILE, BOARDS):
     size = [len(BOARDS[0]), len(BOARDS[0][0])]
-    print(size)
     for brd in BOARDS:
-        for i in range(1, BOARD_SIZE - size[0] - 1):
-            for j in range(1, BOARD_SIZE - size[1] - 1):
-                temp = [[0] * BOARD_SIZE] * BOARD_SIZE
+        for i in range(1, BOARD_SIZE - size[0]):
+            for j in range(1, BOARD_SIZE - size[1]):
+                temp = [[0 for k in range(BOARD_SIZE)] for l in range(BOARD_SIZE)]
                 for l in range(size[0]):
                     for k in range(size[1]):
                         temp[i+l][j+k] = brd[l][k]
                 for l in [0, 1, 2, 3]:
-                    #data_write(FILE, zfy_rotate(temp, l))
-                     print(temp)
-                     print(brd)
-                
+                    data_write(FILE, zfy_rotate(temp, l))
+    return None
     
 #--main------------------------
 def slice(PATH):
